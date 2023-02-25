15d4daf869d8f74147c3513a8bf7050a9920a470
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-02-25 06:05:00 +0000

    Installing TailwindCSS.

--- help.js
+++ help.js
@@ -18,3 +18,29 @@ const res = sortNumbers(4,3,4,6,2,4,1)
 console.log(res)
 
 // R =[1, 2, 3, 4, 4, 4, 6]
+
+
+// ============== Destructuring ====================
+
+const [a, b] = numbers
+const {name, age} = {name: 'Jorge', age: 38}
+
+console.log(a) // 1
+console.log(b) // 2
+
+console.log(name); // Jorge
+console.log(age); // 38
+
+// ============= Primitive Types =================
+
+const firstPerson = { name: 'John', age: 40 }
+
+const secondPerson = firstPerson;
+const thirdPerson = {...firstPerson};
+const fourthPerson = {...firstPerson, country: 'México'};
+
+firstPerson.name = 'Maria'
+
+console.log(secondPerson); // { name: 'Maria', age: 40 }
+console.log(thirdPerson); // { name: 'John', age: 40 }
+console.log(fourthPerson); // { name: 'John', age: 40, country: 'México' }
